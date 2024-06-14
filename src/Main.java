a65b7da924147c5429dc2f10887c51b4b258879b
Dmitry <dmitrykira@ya.ru>
2024-06-14 13:41:16 +0400

    add lesson 3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //F = C * 1.8 + 32
+        /*System.out.println("Температура по фаренгейту" + 32);
+        System.out.println(32 + " C " + (32 * 1.8 + 32) + " F ");*/
+
+        System.out.println(89 + "рублей это: " + (89 * 1/62.08) + " долларов");
+        System.out.println(95 + "рублей это: " + (95 * 1/60.08) + " евро");
+    }
+}
